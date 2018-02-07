af1b0ec236f60d45a43ce546f9140a8ec28a3471
Abdalla Ahmed Talaat <b.abdallaahmed@gmail.com>
2018-02-08 00:52:37 +0200

    updating atm.py
    added some comments, and added 1 to accepted banknotes

--- atm.py
+++ atm.py
@@ -10,15 +10,22 @@ example: atm(500, 215) should return:
 """
 def atm(money, request):
     """
-    prints the banknote value returned by atm for a certain request when atm has money
+    prints banknote value returned by atm for a request if atm has money
+    this works with INTGERS ONLY
     """
-    accepted_notes = [100, 50, 10, 5]
+    # local variables
+    accepted_notes = [100, 50, 10, 5, 1]
+
+    # asserts
     assert request < money, "atm doesn't have enough money"
+    assert type(request) == type(1), "request must be an INTGER"
+
+    # main loop
     for note in accepted_notes:
         while request >= note:
             request -= note
-            print "give ", note
+            print "give", note
 
 
 
-atm(400, 85)
+atm(400, 86)
