5cbdc0e72aa5d79651ba1b4ebca104a41af1751b
Abdalla Ahmed Talaat <b.abdallaahmed@gmail.com>
2018-02-09 15:49:54 +0200

    Updating atm.py #2
    change func name to (withdraw), change asserts into if statements.

--- atm.py
+++ atm.py
@@ -1,14 +1,14 @@
 """
 This is basically practicing some python ~
 atm.py
-contains function atm that prints the banknotes returned by atm
+contains function withdraw that prints the banknotes returned by atm
 example: atm(500, 215) should return:
     give 100
     give 100
     give 10
     give 5
 """
-def atm(money, request):
+def withdraw(money, request):
     """
     prints banknote value returned by atm for a request if atm has money
     this works with INTGERS ONLY
@@ -16,16 +16,19 @@ def atm(money, request):
     # local variables
     accepted_notes = [100, 50, 10, 5, 1]
 
-    # asserts
-    assert request < money, "atm doesn't have enough money"
-    assert type(request) == type(1), "request must be an INTGER"
+    # validating input
+    if not type(request) == type(1):
+        print "request must be an INTGER"
+        return
+    if not type(money) == type(1):
+        print "money must be an INTGER"
+        return
+    if not request < money:
+        print "atm doesn't have enough money"
+        return
 
     # main loop
     for note in accepted_notes:
         while request >= note:
             request -= note
             print "give", note
-
-
-
-atm(400, 86)
