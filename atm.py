a49fb5a2d44b3fc186e8f1f1d32e8a4a950f50af
Abdalla Ahmed Talaat <b.abdallaahmed@gmail.com>
2018-02-10 17:34:42 +0200

    add atm class

--- atm.py
+++ atm.py
@@ -1,34 +1,51 @@
 """
 This is basically practicing some python ~
 atm.py
-contains function withdraw that prints the banknotes returned by atm
+contains atm class
 """
-def withdraw(money, request):
+class atm:
     """
-    prints banknote value returned by atm for a request if atm has money
-    this works with INTGERS ONLY
-    returns the rest of money in balance
+    initializes atm objcet. Can withdraw money
     """
-    # local variables
-    accepted_notes = [100, 50, 10, 5, 1]
+    def __init__(self, bank_name, balance):
+        self._balance = balance
+        self._bank_name = bank_name
+        print "atm for: ({}) initialized successfully with balance: ({}).\n".format(self._bank_name, self._balance)
 
-    # validating input
-    if not type(request) == type(1):
-        print "request must be an INTGER"
-        return
-    if not type(money) == type(1):
-        print "money must be an INTGER"
-        return
-    if not request < money:
-        print "atm doesn't have enough money"
-        return
+    def withdraw(self, request):
+        """
+        prints banknote value returned by atm for a request if atm has money
+        this works with INTGERS ONLY
+        returns the rest of money in balance
+        """
+        # local variables
+        accepted_notes = [100, 50, 10, 5, 1]
+        # validating input
+        if not type(request) == type(1):
+            print "request must be an INTGER"
+            return
+        if not request < self._balance:
+            print "atm doesn't have enough money"
+            return
 
-    print "Current balance:", money
+        print "============================"
+        print "Welcome to {} atm".format(self._bank_name)
+        print "Current balance:", self._balance
+        print "Withdraw Value:", request
+        print "============================"
 
-    # main loop
-    for note in accepted_notes:
-        while request >= note:
-            request -= note
-            print "give", note
+        self._balance -= request
+        # main loop
+        for note in accepted_notes:
+            while request >= note:
+                request -= note
+                print "give", note
 
-    return money - request
+        print "============================"
+        print "balance after withdraw:", self._balance
+        print "============================\n"
+
+atm1 = atm("a bank", 1000)
+atm1.withdraw(355)
+atm1.withdraw(355)
+atm1.withdraw(355)
