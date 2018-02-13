d58a0e32b4ce48684d9f1cb1dec5910caf6b33ee
Abdalla Ahmed Talaat <b.abdallaahmed@gmail.com>
2018-02-13 17:02:32 +0200

    Updating classes in fourm

--- fourms/models.py
+++ fourms/models.py
@@ -4,7 +4,7 @@ class Member:
         self.__id = mem_id
         self._name = name
         self._age = age
-        self.posts = {}
+        self.__posts = {}
 
     def __str__(self):
         return "\nName: {}\nAge: {}\n".format(self._name, self._age)
@@ -15,18 +15,27 @@ class Member:
     def edit_age(self, new_age):
         self._age = new_age
 
-    def new_post(self, post_id, title, main_post):
-        self.posts[post_id] = Post(self.__id, self._name, title, main_post)
-        return self.posts[post_id]
+    def get_posts(self):
+        return self.__posts
 
-    def del_post(self, post_id):
-        del self.posts[post_id]
+    def new_post(self, post_id, title, main_post):
+        self.__posts[post_id] = Post(post_id, self.__id, self._name, title, main_post)
+        return self.__posts[post_id]
+
+    def del_post(self, post_id, post):
+        if post_id in self.__posts.keys():
+            del self.__posts[post_id]
+            del post
+            print "Delete Successful!\n"
+        else:
+            print "You Can't delete this post.\n"
 
 
 
 class Post:
     """docstring for Post."""
-    def __init__(self, owner_id, owner_name, title, main_post):
+    def __init__(self, post_id, owner_id, owner_name, title, main_post):
+        self.__post_id = post_id
         self._owner_id = owner_id
         self._owner_name = owner_name
         self._title = title
@@ -35,6 +44,9 @@ class Post:
 
     def __str__(self):
         return "POST\nWritten By: {}\nTitle: {}\nPost: {}\n".format(self._owner_name, self._title, self._main_post)
+
+    def get_id(self):
+        return self.__post_id
 
     def edit_post(self, member, new_title, new_post):
         if member != self._owner_id:
--- main.py
+++ main.py
@@ -4,6 +4,4 @@ mem0 = m.Member(0, "mem0", 13)
 mem1 = m.Member(1, "mem1", 34)
 mem2 = m.Member(2, "mem2", 23)
 for i in range(10):
-    mem0.new_post(i, "POST TITLE", "POST CONTENT")
-
-print mem0.posts[0]
+    exec('post' + str(i) + '= mem0.new_post(i, "POST TITLE", "POST CONTENT")')
