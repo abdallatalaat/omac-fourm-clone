0a48e2f942e5ba34bc7c60efd2b6e986c6f77c04
Abdalla Ahmed Talaat <b.abdallaahmed@gmail.com>
2018-03-01 23:25:51 +0200

    Adding flask templates

--- fourms/flask_test.py
+++ fourms/flask_test.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 @app.route("/index")
 def home():
-     return "Hi there"
+     return render_template("index.html")
 
 @app.route("/SayHello/<name>")
 def say_hello(name):
