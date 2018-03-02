485390bc96075e62460832afcd10b43324fcdb88
Abdalla Ahmed Talaat <b.abdallaahmed@gmail.com>
2018-03-02 23:41:54 +0200

    editing flask template

--- fourms/test.py
+++ fourms/test.py
@@ -1,13 +1,1 @@
-from flask import Flask, render_template
-app = Flask(__name__)
-
-@app.route("/")
-@app.route("/index")
-def home():
-     return render_template("index.html")
-
-@app.route("/SayHello/<name>")
-def say_hello(name):
-    return "Hello " + name
-
-app.run()
+print ("sf\n" * 54)
